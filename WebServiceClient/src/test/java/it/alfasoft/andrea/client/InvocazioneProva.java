133b71c70ab86643bc4de6180132ea20f573d4f8
and86 <andrea.ln@tiscali.it>
2016-11-21 15:14:04 +0100

    esempi client

---
+++ WebServiceClient/src/test/java/it/alfasoft/andrea/client/InvocazioneProva.java
@@ -0,0 +1,20 @@
+package it.alfasoft.andrea.client;
+
+import it.alfasoft.andrea.model.Dipendente;
+
+import javax.ws.rs.core.Response;
+
+public class InvocazioneProva {
+
+	public static void main(String[] args) {
+		
+		Invocazioni invocazione=new Invocazioni();
+		Response risposta=invocazione.richiestaDipendenteByCode("cod001")
+					.invoke();
+		
+		Dipendente d=risposta.readEntity(Dipendente.class);
+		System.out.println(d.getNome()+" "+d.getCognome()+" "+d.getStipendio()   );
+
+	}
+
+}
---
+++ WebServiceClient/src/test/java/it/alfasoft/andrea/client/ProvaClientPrimo.java
@@ -0,0 +1,64 @@
+package it.alfasoft.andrea.client;
+
+import java.util.List;
+
+import it.alfasoft.andrea.model.Dipendente;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+
+public class ProvaClientPrimo {
+
+	public static void main(String[] args) {
+				
+		//Factory pattern, un metodo statico, crea il cliente
+		Client cliente=ClientBuilder.newClient();
+		
+		//Target di base al quale ppoi aggiungo i pezzi
+		WebTarget baseTarget=cliente.target("http://localhost:8085/SecondoEsempioRest/alfasoft");
+		
+		WebTarget dipTarget=baseTarget.path("/dipendenti");
+		
+		WebTarget singoloDipTarget=dipTarget.path("/{codiceDip}");
+		
+//		//Mando una richiesta di tipo get all'indirizzo specificato
+//		Response response=singoloDipTarget
+//						.resolveTemplate("codiceDip", "cod001")
+//						.request()
+//						.get();
+//		
+//		Dipendente d=response.readEntity(Dipendente.class);
+//		
+//		System.out.println(d.getNome()+" "+d.getCognome());
+//		
+//		//request post per dipendente
+//		Dipendente d2=new Dipendente("aaa","bbb",1200,"cod010");
+//		Response responsePost=dipTarget
+//				.request()
+//				.post(Entity.json(d2));
+//		if(responsePost.getStatus()!=201){
+//			System.out.println("Qualcosa è andato male");
+//		}
+//		System.out.println(responsePost.getStatus());
+		
+		
+		Response response= dipTarget
+				.request()
+				.get();
+		
+		List<Dipendente> dipendenti=response.readEntity(new GenericType<List<Dipendente>>(){});
+		
+		for(Dipendente dd:dipendenti){
+			System.out.println(dd.getNome()+" "+dd.getCognome()+" "+dd.getStipendio());
+		}
+							
+		
+	
+
+	}
+
+}
